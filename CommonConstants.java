5809b8f99aa1d6693ce3d8e938336e835de83f34
anoopmallikarjuna <anoopmallikarjuna434@gmail.com>
2025-02-11 22:18:17 +0530

    Add files via upload

---
+++ CommonConstants.java
@@ -0,0 +1,21 @@
+package calculator;
+
+public class CommonConstants {
+    //unary operators
+    public static final String OPERATOR_PERCENT = "%";
+    public static final String OPERATOR_RECIPROCAL = "1/x";
+    public static final String OPERATOR_SQUARE = "x^2";
+    public static final String OPERATOR_SQRT = "Rt(x)";
+    public static final String OPERATOR_NEGATE = "+/-";
+
+    //other buttons
+    public static final String CLEAR_ENTRY_BUTTON = "CE";
+    public static final String CLEAR_BUTTON = "C";
+    public static final String DEL_BUTTON = "Del";
+
+    //binary operators
+    public static final String OPERATOR_ADD = "+";
+    public static final String OPERATOR_SUB = "-";
+    public static final String OPERATOR_MUL = "x";
+    public static final String OPERATOR_DIV = "/";
+}
